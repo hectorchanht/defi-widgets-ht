f6a6a247e6c1bdac066704f41ca9ecb397e1abb2
BlackChar92 <Kim.xl@protonmail.com>
2022-08-24 11:54:08 +0800

    format

--- defi-demo/src/components/menu.js
+++ defi-demo/src/components/menu.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Menu() {
-    return (
-        <section className="menu">
-            <Link to="/connector">Connector</Link>
-            <Link to="/contract">Contract-Interact</Link>
-            <Link to="/transaction">Transaction-Confirm</Link>
-            <Link to="/tokenlist">Token-List</Link>
-            <Link to="/signsteps">Sign-Steps</Link>
-        </section>
-    )
+  return (
+    <section className="menu">
+      <Link to="/connector">Connector</Link>
+      <Link to="/contract">Contract-Interact</Link>
+      <Link to="/transaction">Transaction-Confirm</Link>
+      <Link to="/tokenlist">Token-List</Link>
+      <Link to="/signsteps">Sign-Steps</Link>
+    </section>
+  )
 }
 
 export default Menu;
