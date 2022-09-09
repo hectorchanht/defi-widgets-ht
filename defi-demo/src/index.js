4ab1a3fc0cc71e29a3ed18989dbf8f7e2e0e1e4b
BlackChar92 <Kim.xl@protonmail.com>
2022-09-09 15:39:54 +0800

    del tokenlist

--- defi-demo/src/index.js
+++ defi-demo/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Connector from './routes/connector';
 import Contract from './routes/contract';
 import Transaction from './routes/transaction';
-import TokenList from './routes/tokenList';
 import SignSteps from './routes/signSteps';
 import './index.css';
 
@@ -16,8 +15,7 @@ render(
             <Route path="connector" element={<Connector />} />
             <Route path="contract" element={<Contract />} />
             <Route path="transaction" element={<Transaction />} />
-            <Route path="tokenlist" element={<TokenList />} />
-            <Route path="signsteps" element={<SignSteps />} />
+=            <Route path="signsteps" element={<SignSteps />} />
         </Routes>
     </BrowserRouter>,
     rootElement
